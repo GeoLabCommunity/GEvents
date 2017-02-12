d9216ba74b9d4efc6dee722e154f79d4f63d4577
WilliamMarshal <vazha.bichiashvili04@geolab.edu.ge>
2017-02-12 17:06:13 +0400

    Added CoordinatorLayout and back button to the event details activity

--- app/src/main/java/ge/edu/geolab/gevents/ui/DetailsPageActivity.java
+++ app/src/main/java/ge/edu/geolab/gevents/ui/DetailsPageActivity.java
@@ -1,7 +1,12 @@
 package ge.edu.geolab.gevents.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatImageButton;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -12,7 +17,7 @@ import java.util.Date;
 import ge.edu.geolab.gevents.R;
 import ge.edu.geolab.gevents.ui.widgets.DateView;
 
-public class DetailsPageActivity extends AppCompatActivity implements OnMapReadyCallback{
+public class DetailsPageActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private MapFragment map;
 
@@ -21,15 +26,28 @@ public class DetailsPageActivity extends AppCompatActivity implements OnMapReady
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details_page);
 
-        map = (MapFragment)getFragmentManager().findFragmentById(R.id.map);
-        map.getMapAsync(this);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        DateView dateView = (DateView) findViewById(R.id.event_date);
-        dateView.setDate(new Date());
+        final AppCompatImageButton backButton = (AppCompatImageButton) (findViewById(R.id.back_btn));
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view){
+                Intent intent = new Intent(DetailsPageActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+            map = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+            map.getMapAsync(this);
+
+            DateView dateView = (DateView) findViewById(R.id.event_date);
+            dateView.setDate(new Date());
+        }
+
+        @Override
+        public void onMapReady (GoogleMap googleMap){
+
+        }
     }
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-
-    }
-}
